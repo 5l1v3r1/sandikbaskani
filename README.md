19e91b7e26e1359f65fb4c9615861937215d4832
Bora Demir ( @yelloware ) <extern.bora-demir@allianz.com.tr>
2019-04-02 15:57:57 +0300

    markdown formatted output

--- README.md
+++ README.md
@@ -17,28 +17,32 @@ bu repodaki program ile sonuçlar otomatik olarak ayıklanmıştır.
 	* CHP oyu sıfır ise dağıtılmamış tüm oyları CHP hanesine yazar.
 * Sorunlu sandıklar için sonuçları ekrana basar.
 
-2019-04-02 15:21:39.183 | http proxy initialized..
 
-2019-04-02 15:33:15.425 | İSTANBUL/ARNAVUTKÖY/1524 sandık sonuçları:
-
-2019-04-02 15:33:15.425 | 	Ysk zamanı 31.03.2019 22:58:25
-
-2019-04-02 15:33:15.425 | 	Kullanılan oy sayısı 288
-
-2019-04-02 15:33:15.425 | 	Geçerli oy sayısı 280
-
-2019-04-02 15:33:15.425 | 	Geçersiz oy sayısı 8
-
-2019-04-02 15:33:15.425 | 	İşlenmemiş oy sayısı 3
-
-2019-04-02 15:33:15.425 | 	CHP 0
-
-2019-04-02 15:33:15.425 | 	AKP 154
-
-2019-04-02 15:33:15.425 | 	Hatalar:
-
-2019-04-02 15:33:15.425 | 	**CHP oyları sıfır
 ----------
 
-
-2019-04-02 15:21:40.324 | Giriş yapıldı
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1001	|	31.03.2019 23:52:48	|	289	|	265	|	24	|	21	|	0	|	135	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1002	|	31.03.2019 23:05:56	|	299	|	277	|	22	|	7	|	0	|	159	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1003	|	31.03.2019 22:59:54	|	296	|	281	|	15	|	21	|	0	|	138	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1004	|	1.04.2019 00:06:20	|	286	|	272	|	14	|	18	|	0	|	149	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1005	|	31.03.2019 23:41:33	|	295	|	290	|	5	|	19	|	0	|	164	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1006	|	31.03.2019 23:20:51	|	295	|	273	|	22	|	15	|	0	|	156	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1007	|	31.03.2019 23:42:43	|	291	|	270	|	21	|	21	|	0	|	164	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1008	|	31.03.2019 22:54:33	|	296	|	283	|	13	|	24	|	0	|	172	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1009	|	1.04.2019 00:53:32	|	269	|	244	|	25	|	13	|	0	|	156	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1010	|	1.04.2019 01:04:29	|	251	|	235	|	16	|	11	|	0	|	129	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1011	|	1.04.2019 01:04:19	|	261	|	249	|	12	|	13	|	0	|	155	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1012	|	1.04.2019 01:11:46	|	266	|	246	|	20	|	15	|	0	|	133	|	CHP oyları sıfır
+Sandık	|	Ysk Zamanı	|	Kullanılan oy	|	Geçerli oy	|	Geçersiz oy	|	İşlenmemiş oy	|	CHP	|	AKP	|	Hata
+İSTANBUL/ARNAVUTKÖY/1013	|	1.04.2019 01:38:56	|	265	|	257	|	8	|	7	|	0	|	158	|	CHP oyları sıfır
--- src/main/java/tr/com/yelloware/sandikbaskani/ChpResultParser.java
+++ src/main/java/tr/com/yelloware/sandikbaskani/ChpResultParser.java
@@ -9,12 +9,13 @@ import tr.com.yelloware.sandikbaskani.model.BallotBox;
 import tr.com.yelloware.sandikbaskani.model.BallotBoxResult;
 import tr.com.yelloware.sandikbaskani.model.City;
 import tr.com.yelloware.sandikbaskani.model.District;
-import tr.com.yelloware.sandikbaskani.model.type.PartyType;
 import tr.com.yelloware.sandikbaskani.validation.BallotBoxResultValidator;
 
 @UtilityClass
 @Slf4j
 public class ChpResultParser {
+
+  private static final String MD_COL_SEP = "\t|\t";
 
   public static void main(String[] args) throws IOException, InterruptedException {
     ChpSession chpSession = new ChpSession();
@@ -37,23 +38,50 @@ public class ChpResultParser {
           if (result.getValidationErrorList().isEmpty()) {
             continue;
           }
-          log.info("{} sandık sonuçları:", result.getTitle());
-          log.info("\tYsk zamanı {}", result.getYskReceiveTime());
-          log.info("\tKullanılan oy sayısı {}", result.getTotalVoteCount());
-          log.info("\tGeçerli oy sayısı {}", result.getValidVoteCount());
-          log.info("\tGeçersiz oy sayısı {}", result.getNotValidVoteCount());
-          log.info("\tİşlenmemiş oy sayısı {}", result.getNotMatchVoteCount());
-          log.info("\tCHP {}", result.getChpVoteCount());
-          log.info("\tAKP {}", result.getAmpulVoteCount());
-          log.info("\tHatalar:");
-          if (PartyType.AKP.equals(result.getNotMatchVoteParty())) {
-            akpCount += result.getNotMatchVoteCount();
-          } else if (PartyType.CHP.equals(result.getNotMatchVoteParty())) {
-            chpCount += result.getNotMatchVoteCount();
-          }
-          for (String err : result.getValidationErrorList()) {
-            log.info("\t**{}", err);
-          }
+
+          // First Header | Second Header
+          // ------------- | -------------
+          // Content Cell | Content Cell
+          // Content Cell | Content Cell
+
+          log.info(
+            "Sandık" + MD_COL_SEP +
+              "Ysk Zamanı" + MD_COL_SEP +
+              "Kullanılan oy" + MD_COL_SEP +
+              "Geçerli oy" + MD_COL_SEP +
+              "Geçersiz oy" + MD_COL_SEP +
+              "İşlenmemiş oy" + MD_COL_SEP +
+              "CHP" + MD_COL_SEP +
+              "AKP" + MD_COL_SEP +
+              "Hata");
+          log.info(
+            result.getTitle() + MD_COL_SEP +
+              result.getYskReceiveTime() + MD_COL_SEP +
+              result.getTotalVoteCount() + MD_COL_SEP +
+              result.getValidVoteCount() + MD_COL_SEP +
+              result.getNotValidVoteCount() + MD_COL_SEP +
+              result.getNotMatchVoteCount() + MD_COL_SEP +
+              result.getChpVoteCount() + MD_COL_SEP +
+              result.getAmpulVoteCount() + MD_COL_SEP +
+              result.getValidationErrorList().get(0));
+
+          // log.info("{} sandık sonuçları:", result.getTitle());
+          // log.info("\tYsk zamanı {}", result.getYskReceiveTime());
+          // log.info("\tKullanılan oy sayısı {}", result.getTotalVoteCount());
+          // log.info("\tGeçerli oy sayısı {}", result.getValidVoteCount());
+          // log.info("\tGeçersiz oy sayısı {}", result.getNotValidVoteCount());
+          // log.info("\tİşlenmemiş oy sayısı {}", result.getNotMatchVoteCount());
+          // log.info("\tCHP {}", result.getChpVoteCount());
+          // log.info("\tAKP {}", result.getAmpulVoteCount());
+          // log.info("\tHatalar:");
+          // if (PartyType.AKP.equals(result.getNotMatchVoteParty())) {
+          // akpCount += result.getNotMatchVoteCount();
+          // } else if (PartyType.CHP.equals(result.getNotMatchVoteParty())) {
+          // chpCount += result.getNotMatchVoteCount();
+          // }
+          // for (String err : result.getValidationErrorList()) {
+          // log.info("\t**{}", err);
+          // }
         }
       }
     } finally {
